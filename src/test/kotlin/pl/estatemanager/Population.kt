433dd2ca5f1e492c19a6201338df4675c9b6891a
rogekk <82406267+rogekk@users.noreply.github.com>
2021-06-12 19:28:19 +0200

    added new users and issues to population

--- src/test/kotlin/pl/estatemanager/Population.kt
+++ src/test/kotlin/pl/estatemanager/Population.kt
@@ -2,12 +2,9 @@ package pl.estatemanager.db
 
 import org.joda.time.DateTime
 import pl.estatemanager.models.domain.CommunityId
-import pl.estatemanager.models.domain.domains.Community
-import pl.estatemanager.models.domain.domains.ResolutionCreation
-import pl.estatemanager.models.domain.domains.Shares
-import pl.estatemanager.models.domain.domains.TopicCreation
-import pl.estatemanager.models.domain.domains.VoteCountingMethod
+import pl.estatemanager.models.domain.domains.*
 import pl.estatemanager.repositories.di.RepositoriesModule.communityRepository
+import pl.estatemanager.repositories.di.RepositoriesModule.issueRepository
 import pl.estatemanager.repositories.di.RepositoriesModule.resolutionsRepository
 import pl.estatemanager.repositories.di.RepositoriesModule.topicsRepository
 import pl.estatemanager.repositories.di.RepositoriesModule.usersRepository
@@ -21,7 +18,9 @@ fun main() {
     val communityId2 = communityRepository().createCommunity(Community(CommunityId("id2"), "Kolejowa", (100)))
 
     // create Owners
+    val c1 = listOf(communityId2 to Shares(100))
     val c = listOf(communityId1 to Shares(10))
+
     val user1 = (usersRepository().createOwner(
         communities = c,
         username = "flavio",
@@ -33,14 +32,14 @@ fun main() {
     ) as OwnerCreated).ownerId
 
 
-    val user3 = (usersRepository().createOwner(
-        communities = c,
+    val user2 = (usersRepository().createOwner(
+        communities = c1,
         username = "rojek",
         password = "rojek",
         email = "rojek@gmail.com",
         fullName = "Kamil Rojek",
         phoneNumber = "33434234098",
-        address = "Bankowa"
+        address = "Kolejowa"
     ) as OwnerCreated).ownerId
 
     usersRepository().createOwner(
@@ -69,6 +68,14 @@ fun main() {
     usersRepository().createOwner( c, "LeopoldaZaaa13", "pass", "lepolda@gmail.com", "Leopolda Zampa", "20384", "Croissant" )
     usersRepository().createOwner( c, "potatosteak", "pass", "steaks@gmail.com", "Frau Lolda Standke", "98734", "Salami" )
 
+
+    usersRepository().createOwner(c1,"woody","woody","woody@gmail.com", "Woody Johnson", "123456789", "Kolejowa")
+    usersRepository().createOwner(c1,"steve","steve","steve@gmail.com", "Steve Williams", "987654321", "Kolejowa")
+    usersRepository().createOwner(c1,"ethel","ethel","ethel@gmail.com", "Ethel Anderson", "321654987", "Kolejowa")
+    usersRepository().createOwner(c1,"connie","connie","connie@gmail.com", "Connie Cunnaman", "789456123", "Kolejowa")
+    usersRepository().createOwner(c1,"malloy","malloy","malloy@gmail.com", "Malloy Johnson", "234345456", "Kolejowa")
+    usersRepository().createOwner(c1,"denzel","denzel","denzel@gmail.com", "Denzel Jackson", "456567678", "Kolejowa")
+
     val manager1 = usersRepository().createManager(
         listOf(communityId1),
         "manager",
@@ -91,6 +98,27 @@ fun main() {
         "Kolejowa"
     )
 
+    val manager2 = usersRepository().createManager(
+        listOf(communityId2),
+        "manager2",
+        "manager2",
+        "jsmith@gmail.com",
+        "Manager",
+        "Manageris",
+        "333333333",
+        "Bankowa"
+    )
+
+    val admin2 = usersRepository().createAdmin(
+        listOf(communityId2),
+        "admin2",
+        "admin2",
+        "tnowak@gmail.com",
+        "Admin",
+        "Adminski",
+        "444444444",
+        "Kolejowa"
+    )
     topicsRepository().crateTopic(
         TopicCreation(
             "I want to fire our shity Manager",
@@ -104,7 +132,7 @@ fun main() {
     topicsRepository().crateTopic(
         TopicCreation(
             "Estate management system",
-            user1,
+            user2,
             DateTime.now(),
             communityId2,
             "I would like to fire my manager but this system is soo goood",
@@ -118,6 +146,26 @@ fun main() {
             "That what she said",
             "I don't know",
             VoteCountingMethod.ONE_OWNER_ONE_VOTE,
+        )
+    )
+
+    issueRepository().createIssue(
+        IssueCreation(
+            "there is no light in my apartment",
+            "please bring back the light",
+            "attachments",
+            user1,
+            communityId1
+        )
+    )
+
+    issueRepository().createIssue(
+        IssueCreation(
+            "someone left poop on my doormat",
+            "pls help",
+            "photo of the poop",
+            user2,
+            communityId2
         )
     )
 }
